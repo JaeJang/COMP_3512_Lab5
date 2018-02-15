e7ce176df0d7397b5f45164d315651e6221da03b
Jae Jang <jaejang5566@gmail.com>
2018-02-14 22:52:55 -0800

    AbstractOperation class created

---
+++ RPNCalculator/RPNCalculator/AbstractOperation.hpp
@@ -0,0 +1,12 @@
+#include "Operation.hpp"
+
+class AbstractOperation : public Operation{
+
+private:
+	char operation_type;
+
+public:
+	virtual ~AbstractOperation() { }
+	explicit AbstractOperation(char type) :operation_type(type) { };
+	virtual char  get_code() override { return operation_type; }
+};
